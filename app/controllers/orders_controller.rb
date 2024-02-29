0031065a78d0d83b69a1db21436d87e8db4ae957
Cleiston Amaral <c.alexandresouza@yahoo.com>
2024-02-29 12:48:52 -0300

    creation of ordersController and begining of home.html.erb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,38 @@
 class OrdersController < ApplicationController
+  before_action :set_products, only: [:new, :create]
+
+  def index
+    @order = Order.all
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.products = @products
+    if @order.save
+      redirect_to product_order_path, notice: 'the order was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    order = Order.find(params[:id])
+    product = order.product
+    order.destroy
+    redirect_to product_path(product), notice: 'the order was successfully erased'
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:quantity, :user_id)
+  end
+
+  def set_products
+    @product = Product.find(params[:product_id])
+  end
 end

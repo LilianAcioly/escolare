7fa274ab9fd7c3fa8bc4f85c14dba0b9043e8069
Cleiston Amaral <c.alexandresouza@yahoo.com>
2024-03-04 14:57:26 -0300

    major updates models product, search and navbar

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,13 @@ class PagesController < ApplicationController
 
   def home
     @products = Product.all
-  end
 
-  
+    if params[:query].present?
+      @products = @products.search_by_name_category_and_description(params[:query])
+    end
+
+    if params[:category].present?
+      @products = @products.where(category: params[:category])
+    end
+  end
 end
